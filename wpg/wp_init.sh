a1867ab87f12ada30bd63e443a0eefb638fb2a2e
Nikhil Singh <nik.singh710@gmail.com>
2023-02-13 01:15:33 +0530

    Added multiple stuff

--- wpg/wp_init.sh
+++ wpg/wp_init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wpg -nrs '/home/niksingh710/Pictures/wallpapers/Second/Simplistic/chlorine as a ghost.jpg' '/home/niksingh710/Pictures/wallpapers/Second/Simplistic/chlorine as a ghost.jpg'
+wpg -nrs '/home/niksingh710/Pictures/wallpapers/PersonalWalls/Alena/alena-aenami-you.jpg' '/home/niksingh710/Pictures/wallpapers/PersonalWalls/Alena/alena-aenami-you.jpg'
