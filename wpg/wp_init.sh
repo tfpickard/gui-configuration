4380836a0ef61da4556b8adc314321c976ce6636
Nikhil Singh <nik.singh710@gmail.com>
2023-02-16 22:45:06 +0530

    after firefox

--- wpg/wp_init.sh
+++ wpg/wp_init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wpg -nrs '/home/niksingh710/Pictures/wallpapers/PersonalWalls/e5pmk3avjgn71.png' '/home/niksingh710/Pictures/wallpapers/PersonalWalls/e5pmk3avjgn71.png'
+wpg -nrs '/home/niksingh710/Pictures/wallpapers/First/dropside-the-valley-ultrawide-gradient-landscape.png' '/home/niksingh710/Pictures/wallpapers/First/dropside-the-valley-ultrawide-gradient-landscape.png'
