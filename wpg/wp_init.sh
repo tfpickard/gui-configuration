2ec4da48869257ef8c30f634bd6b203b0aac54c3
Nikhil Singh <nik.singh710@gmail.com>
2023-03-14 17:58:19 +0530

    Added Much stuff switched to alacritty added qt5ct and template modified wpgtk and much more

--- wpg/wp_init.sh
+++ wpg/wp_init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wpg -nrs '/home/niksingh710/Pictures/wallpapers/PersonalWalls/purple-cat.jpg' '/home/niksingh710/Pictures/wallpapers/PersonalWalls/purple-cat.jpg'
+wpg -nrs '/home/niksingh710/Pictures/wallpapers/PersonalWalls/Matrix.jpg' '/home/niksingh710/Pictures/wallpapers/PersonalWalls/Matrix.jpg'
