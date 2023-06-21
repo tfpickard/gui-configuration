4051c96903d51f9ee96541e69aeea7f572351388
Nikhil Singh <nik.singh710@gmail.com>
2023-06-21 20:29:01 +0530

    Regular update

--- wpg/wp_init.sh
+++ wpg/wp_init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wpg -nrs '/home/niksingh710/Pictures/wallpapers/PersonalWalls/golden-night.jpg' '/home/niksingh710/Pictures/wallpapers/PersonalWalls/golden-night.jpg'
+wpg -nrs '/home/niksingh710/Pictures/wallpapers/favwalls/World_Map_4k_Gruvbox_Dark.png' '/home/niksingh710/Pictures/wallpapers/favwalls/World_Map_4k_Gruvbox_Dark.png'
